6389206a8cad5f9392b030150fdb2937305c3b5b
arpitdalal <arpitdalalm@gmail.com>
2022-02-08 09:58:42 -0500

    fix the color style issue

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import { useState } from "react";
 import { hydrate } from "react-dom";
 import { RemixBrowser } from "remix";
-import { CacheProvider, ThemeProvider } from "@emotion/react";
-import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
 
 import ClientStyleContext from "~/context/ClientStyleContext";
 import createEmotionCache from "~/utils/createEmotionCache";
-import theme from "~/themes/dark";
 
 function ClientCacheProvider({ children }: React.PropsWithChildren<{}>) {
   const [cache, setCache] = useState(createEmotionCache());
@@ -25,11 +23,11 @@ function ClientCacheProvider({ children }: React.PropsWithChildren<{}>) {
 
 hydrate(
   <ClientCacheProvider>
-    <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <RemixBrowser />
-    </ThemeProvider>
+    {/* <ThemeProvider theme={theme}> */}
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    {/* <CssBaseline /> */}
+    <RemixBrowser />
+    {/* </ThemeProvider> */}
   </ClientCacheProvider>,
   document
 );
